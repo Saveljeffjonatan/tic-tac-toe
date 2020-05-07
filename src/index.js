6e97b9545adf1df17205caa33d092a736109e1ae
Jonatan Saveljeff <Saveljeffjonatan@gmail.com>
2020-05-07 15:22:57 +0200

    added time travel to the game

--- src/index.js
+++ src/index.js
@@ -13,48 +13,18 @@ import './index.css';
   
   
   class Board extends React.Component {
-    constructor(props) {
-        super(props);
-         this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    } // Checks if 'x' is true or false and fills the squares with null.
-
-    handleClick(i) {
-      const squares = this.state.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-          this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-      });
-    } // If a winner is determined then 'handleClick' stops else it continues to work.
-
     renderSquare(i) {
       return (
         <Square 
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
         />
       );
     } // Renders the squares as null or gives them a value if 'onClick' has been handled.
   
-    render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if (winner) {
-        status = 'Winner: ' + winner;
-      } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      } // Checks if there is a winner otherwise it tells you which player is next.
-      
-  
-      return (
+      render() {
+        return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -76,15 +46,75 @@ import './index.css';
   }
   
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+        this.state = {
+          history: [{
+            squares: Array(9).fill(null)
+          }],
+          stepNumber: 0,
+          xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
+    }  // If a winner is determined then 'handleClick' stops else it continues to work.
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
+
     render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+      
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick = {() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      } // Checks what move has been made or if a winner is declared
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+              squares = {current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
@@ -92,9 +122,9 @@ import './index.css';
   } 
 
 
-  
   // ========================================
   
+
   ReactDOM.render(
     <Game />,
     document.getElementById('root')
@@ -113,9 +143,9 @@ import './index.css';
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[a] && squares[c]) {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a]) {
         return squares[a];
       }
-    }
+    }  
     return null;
   } // Calculates if there is 3 in a row or not. If 3 is found then it calculates that player as a winner.
